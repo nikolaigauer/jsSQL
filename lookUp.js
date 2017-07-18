63521881c625bc0309b049363fee1b3095641cfa
Nikolai <nikolaigauer@gmail.com>
2017-07-18 20:15:54 +0000

    add lookUp function

---
+++ lookUp.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people", (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log("Found one person(s) with the name " + process.argv[2] + result); //output: 1
+    client.end();
+  });
+});
