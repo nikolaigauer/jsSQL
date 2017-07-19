ea47ada5484662e000b43643f481d8d1b5b7d00d
Nikolai <nikolaigauer@gmail.com>
2017-07-19 01:35:48 +0000

    add implementation of js query function

--- lookUp.js
+++ lookUp.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+const nameInput = process.argv[2];
 
 const client = new pg.Client({
   user     : settings.user,
@@ -10,15 +11,25 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
+client.connect((error) => {
+  if (error) {
+    return console.error("Connection Error", error);
   }
-  client.query("SELECT * FROM famous_people", (err, result) => {
+  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [nameInput], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log("Found one person(s) with the name " + process.argv[2] + result); //output: 1
+
+    if (result.rowCount != 0) {
+      for (var i = 0; i < result.rows.length; i++) {
+        var dob = String(result.rows[i].birthdate);
+        console.log("Searching...");
+        console.log("Found " + (i + 1) + " person(s) with the name " + "'" + nameInput + "':");
+        console.log("- " + result.rows[i].id + ": " + result.rows[i].first_name, result.rows[i].last_name + ", born " + "'" + dob.substring(0,15) + "'");
+      }
+    } else {
+      console.log("No one by that name in our database")
+    }
     client.end();
   });
 });
